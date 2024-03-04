d062813f5e79a043a3542d23358d163376145bc3
Daniella <daniella@Daniellas-MacBook-Pro.local>
2024-03-04 14:12:34 -0600

    intro to JS, added message with popup for combination

---
+++ main.js
@@ -0,0 +1,33 @@
+/* PSEUDOCODE 
+- open vs code
+- mkdir index.html & main.js 
+- link index and main 
+- create string variable with intro message 
+- create the variable with 10, 40, 39 
+- use alert box to display message and codes 
+
+*/ 
+
+
+// String with intro message 
+
+const intro = "You have recieved this message because you have been chosen to open an important vault. Here is the secret combination: "
+
+// variables that hold numbers  no= number
+
+const no1 = 5; 
+const no2 = 45; 
+const no3 = 38; 
+
+// arithmetic ops combo= combination
+
+const combo1 = no1 * 2;
+const combo2 = no2 - 5;
+const combo3 = no3 + 1; 
+
+// string variable for display message 
+
+const displayMessage = intro + combo1 + " " + combo2 + " "+ combo3
+
+// alert message 
+alert(displayMessage)
